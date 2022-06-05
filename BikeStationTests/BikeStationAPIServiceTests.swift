5d507fe60d282ceb0fa18affa190b00cc7765261
Santosh Khandare <santosh59@gmail.com>
2022-06-05 15:03:48 +0530

    test cases for valid invlida api response

--- BikeStationTests/BikeStationAPIServiceTests.swift
+++ BikeStationTests/BikeStationAPIServiceTests.swift
@@ -11,6 +11,8 @@ import Combine
 
 class BikeStationAPIServiceTests: XCTestCase {
 
+    private var cancellable: Cancellable?
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,10 +21,80 @@ class BikeStationAPIServiceTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func test_getFromURL_succeedsOnHTTPURLResponseWithData() {
+        let data = makeData()
+        let response = HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)!
+        URLProtocolStub.stub(data: data, response: response, error: nil)
+        
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for completion")
+        
+        let result = sut.get(from: anyURL())
+        cancellable = result.sink { error in
+            switch error {
+                case .finished:
+                    print("success")
+                case .failure(let error):
+                    XCTFail("Expected successful api result, got \(error) instead")
+            }
+        } receiveValue: { values in
+            XCTAssertEqual(values.count, 179, "Expected valid data from API")
+            exp.fulfill()
+        }
 
+        wait(for: [exp], timeout: 2.0)
     }
+    
+    func test_getFromURL_failsOnAllInvalidResponseCases() {
+        let data = Data()
+        let response = HTTPURLResponse(url: anyURL(), statusCode: 400, httpVersion: nil, headerFields: nil)!
+        URLProtocolStub.stub(data: data, response: response, error: nil)
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for completion")
+        
+        let result = sut.get(from: anyURL())
+        cancellable = result.sink { error in
+            switch error {
+                case .finished:
+                    print("success")
+                case .failure(let error):
+                XCTAssertEqual(error.localizedDescription, APIServiceError.serverError.localizedDescription)
+                    exp.fulfill()
+            }
+        } receiveValue: { values in
+        }
+        wait(for: [exp], timeout: 2.0)
+    }
+    
+    private func makeSUT() -> APIService {
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        
+        let sut = BikeStationAPI(session: session)
+        return sut
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
+    
+    private func makeData() -> Data {
+        let data: Data
+        let filename = "bikeStationData"
+        let bundle = Bundle(for: Self.self)
+        guard let file = bundle.path(forResource: filename, ofType: "json")
+            else {
+                fatalError("Couldn't find \(filename) in main bundle.")
+        }
 
+        do {
+            data = try Data(contentsOf: URL(fileURLWithPath: file))
+        } catch {
+            fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+        }
+        return data
+    }
 
 }
 
@@ -47,5 +119,37 @@ enum APIServiceError: Error {
         case .parsing:
             return "The server returned data in an unexpected format. Try updating the app."
         }
+    }
+}
+
+final class BikeStationAPI: APIService {
+    private let session: URLSession
+    public init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) -> AnyPublisher<[BikeStation], Error> {
+        return session.dataTaskPublisher(for: url)
+            .mapError { error -> Error in
+                return APIServiceError.networkError(error)
+            }
+            .tryMap { output in
+                guard let urlResponse = output.response as? HTTPURLResponse else {
+                    throw APIServiceError.invalidResponse
+                }
+                guard self.isOK(urlResponse) else {
+                    throw APIServiceError.serverError
+                }
+                return output.data
+            }
+            .tryMap { data in
+                return try BikeStationMapper.map(data)
+            }
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+    
+    private func isOK(_ response: HTTPURLResponse) -> Bool {
+        (200...299).contains(response.statusCode)
     }
 }
