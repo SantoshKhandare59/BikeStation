6faae3cb88eb7306ce7e36493cad1e1b396fce12
Santosh Khandare <santosh59@gmail.com>
2022-06-05 13:46:25 +0530

    test case for valid and invalid json parsing

--- BikeStationTests/BikeStationDataMapperTests.swift
+++ BikeStationTests/BikeStationDataMapperTests.swift
@@ -18,19 +18,38 @@ class BikeStationDataMapperTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_bikestation_successOnvalidJSON() {
+        let data = makeData()
+        guard let result = try? BikeStationMapper.map(data) else {
+            fatalError("Couldn't find parse data.")
         }
+        XCTAssertFalse(result.isEmpty, "Expected non-empty json data")
+    }
+    
+    func test_bikestation_throwsErrorOnInvalidJSON() {
+        let invalidJSON = Data("invalid json".utf8)
+        
+        XCTAssertThrowsError(
+            try BikeStationMapper.map(invalidJSON)
+        )
+    }
+    
+    
+    private func makeData() -> Data {
+        let data: Data
+        let filename = "bikeStationData"
+        let bundle = Bundle(for: Self.self)
+        guard let file = bundle.path(forResource: filename, ofType: "json")
+            else {
+                fatalError("Couldn't find \(filename) in main bundle.")
+        }
+
+        do {
+            data = try Data(contentsOf: URL(fileURLWithPath: file))
+        } catch {
+            fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+        }
+        return data
     }
 
 }
