31805a37a2ede8272d8b1bbdb147ceb32b8c8fc3
Santosh Khandare <santoshk@aymcommerce.com>
2022-06-05 12:23:50 +0530

    Initial Commit

---
+++ Shared/BikeStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeStationApp.swift
+//  Shared
+//
+//  Created by ft_admin on 05/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BikeStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
