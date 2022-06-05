0d0a586b923a4e32d8b911314c05e8e99419206f
Santosh Khandare <santosh59@gmail.com>
2022-06-05 13:40:13 +0530

    write logic for json parsing

--- BikeStationTests/BikeStationDataMapperTests.swift
+++ BikeStationTests/BikeStationDataMapperTests.swift
@@ -57,10 +57,25 @@ final class BikeStationMapper {
         }
         private let type: String
         private let features: [Feature]
+        
+        var stations: [BikeStation] {
+            features.map {
+                BikeStation(id: $0.id, name: $0.properties.label, bikes: Int($0.properties.bikeRacks) ?? 0, availablebikes: Int($0.properties.bikes) ?? 0, coordinates: BikeStation.Coordinates(latitude: $0.geometry.coordinates[0], longitude: $0.geometry.coordinates[1]))
+            }
+        }
     }
     
     public enum Error: Swift.Error {
         case invalidData
+    }
+    
+    static func map(_ data: Data) throws -> [BikeStation] {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(DateFormatter.yyyyMMdd)
+        guard let root = try? decoder.decode(Root.self, from: data) else {
+            throw Error.invalidData
+        }
+        return root.stations
     }
 }
 
@@ -81,4 +96,12 @@ struct BikeStation: Identifiable {
         var latitude: Double
         var longitude: Double
     }
+}
+
+extension DateFormatter {
+    static let yyyyMMdd: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd HH:mm"
+        return formatter
+    }()
 }
