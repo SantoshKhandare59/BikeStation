93f0b08f828bf6f1752b1fe5aaf83e14548e1bc5
Santosh Khandare <santosh59@gmail.com>
2022-06-05 15:32:49 +0530

    Remove view model tests

--- BikeStationTests/BikeStationAPIServiceTests.swift
+++ BikeStationTests/BikeStationAPIServiceTests.swift
@@ -15,6 +15,7 @@ class BikeStationAPIServiceTests: XCTestCase {
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        URLProtocolStub.removeStub()
     }
 
     override func tearDownWithError() throws {
--- Shared/ViewModel/BikeStationViewModel.swift
+++ Shared/ViewModel/BikeStationViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import Combine
+
+final class BikeStationViewModel: ObservableObject {
+    @Published var data = [BikeStation]()
+    @Published var state: ResultState = .loading
+    private let service: APIService
+    private var cancellable: Cancellable?
+    
+    enum ResultState {
+        case loading
+        case success
+        case failure(String)
+    }
+    
+    init(service: APIService) {
+        self.service = service
+    }
+    
+    func fetchBikeStations() {
+        self.state = .loading
+        let url = BikeStationEndpoint.get(mtype: "pub_transport", co: "stacje_rowerowe").url(baseURL: Constants.baseURL)
+        let result = service.get(from: url)
+        cancellable = result.sink { error in
+            switch error {
+                case .finished:
+                    self.state = .success
+                case .failure(let error):
+                    self.state = .failure(error.localizedDescription)
+            }
+        } receiveValue: { values in
+            self.data = values
+        }
+    }
+}
