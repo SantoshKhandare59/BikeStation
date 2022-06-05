df6d875a7f37be5634a1b51b1365b19c6f0c0c46
Santosh Khandare <santosh59@gmail.com>
2022-06-05 15:47:58 +0530

    create ui for show map, pin, annotate

--- Shared/Views/BikeStationDetailView.swift
+++ Shared/Views/BikeStationDetailView.swift
@@ -6,11 +6,38 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct BikeStationDetailView: View {
     let station: BikeStation
+    @State private var region = MKCoordinateRegion()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Map(coordinateRegion: $region, showsUserLocation: true, annotationItems: [station]) { loc in
+                MapAnnotation(coordinate: loc.locationCoordinate) {
+                    ZStack {
+                        Color.white
+                        Image("Bike").resizable()
+                            .frame(width: 15, height: 15).padding(5)
+                    }
+                    .cornerRadius(10)
+                    Text("\(station.bikes)").foregroundColor(.green)
+                }
+            }
+            .ignoresSafeArea(edges: .top)
+            BikeStationListRow(station: station)
+        }
+        .onAppear {
+            setRegion()
+        }
+    }
+    
+    private func setRegion() {
+        region = MKCoordinateRegion(
+            center: station.locationCoordinate,
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
     }
 }
 
